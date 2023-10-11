f50bb9eb9eb337423f2e59ecce36eaca476eaf20
Stephen Dice <jetayor@gmail.com>
2023-10-11 16:11:20 +0000

    third initial commit

--- HotelMgmtSystem.py
+++ HotelMgmtSystem.py
@@ -3,56 +3,338 @@ from datetime import date
 class Hotel:
 
 
-	def __init__(self, hotel_name, location,rooms_availability, total_rooms):
+	def __init__(self, hotel_name, location):
 		self.hotel_name = hotel_name 
 		self.location = location 
-		self.rooms_availability = rooms_availability
-		self.total_rooms = total_rooms
+		
 
 
 class Five_Star_Hotel(Hotel):
-	def __init__(self, hotel_name, location, rooms_availability, rating):
-		super().__init__(hotel_name, location, rooms_availability)
-		self.rating = "Five Star"
-
+	def __init__(self, hotel_name, location, rooms_availability):
+		super().__init__(hotel_name, location)
+		self.rooms_availability = rooms_availability
+		
 
 
 class Four_Star_Hotel(Hotel):
-	def __init__(self, hotel_name, location, rooms_availability, rating):
-		super().__init__(hotel_name, location, rooms_availability)
-		self.rating = "Four Star"
+	def __init__(self, hotel_name, location, rooms_availability):
+		super().__init__(hotel_name, location)
+		self.rooms_availability = rooms_availability
+		
+		
 
 class Three_Star_Hotel(Hotel):
-	def __init__(self, hotel_name, location, rooms_availability, rating):
+	def __init__(self, hotel_name, location, rooms_availability):
+		super().__init__(hotel_name, location)
+		self.rooms_availability = rooms_availability
+		
+		
+
+
+class Kempinski_Hotel(Five_Star_Hotel):
+
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
 		super().__init__(hotel_name, location, rooms_availability)
-		self.rating = "Three Star"
+		self.room_status = room_status 
+		self.room_price = {"Presidential Suite" : 6000,
+						 	"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+											 }
+						
+		self.room_type = {'Presidential Suite' :[4501, 4507, 4519],
+							"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+
+						}
+
+class Accra_Marriott_Hotel(Five_Star_Hotel):
+
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Presidential Suite" : 6000,
+						 	"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+											 }
+						
+		self.room_type = {'Presidential Suite' :[4501, 4507, 4519],
+							"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+										}
+
+class Movenpick_Ambassador(Five_Star_Hotel):
+
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Presidential Suite" : 6000,
+							"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+											 }
+						
+		self.room_type = {'Presidential Suite' :[4501, 4507, 4519],
+							"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+
+											}			
+
+class Labadi_Beach_Hotel(Five_Star_Hotel):
+
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Presidential Suite" : 6000,
+							"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+											 }
+						
+		self.room_type = {'Presidential Suite' :[4501, 4507, 4519],
+							"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+						}
+
+class Tang_Hotel(Five_Star_Hotel):
+
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Presidential Suite" : 6000,
+							"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+						
+		self.room_type = {'Presidential Suite' :[4501, 4507, 4519],
+							"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+						}
+						 
+
+class Lancaster_Kumasi_City(Four_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+						}
+							
+
+class Lancaster_Accra(Four_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+						}
+							
+
+class La_Palm_RoyalBeach(Four_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+						}
+							
+
+class Kwarleyz_Residence(Four_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136] 
+							}
+		
+class Villa_Monticello_Boutique_Hotel(Four_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Execuetive Suite" : 5000 ,
+							"Junior Suite" : 4000 ,
+							"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Execuetive Suite" : [3923, 3955, 3984],
+							"Junior Suite": [2564, 2533, 2521],
+							"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+						}
+
+class Zaina_Lodge(Three_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+
+		}
+							
+
+class Orchid_Hotel_EastLegon(Three_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
+
+		}
+							
 
 
-class Room:
+class Orchid_Hotel_EastLegon(Three_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
 
-	 def __init_(self, room_number, room_type, room_price, room_status):
-						self.room_number = room_number 
-						self.room_type = room_type 
-						self.room_price = room_price 
-						self.room_status = room_status 
+		}
 
-class Presidential_Suite(Room):
-	    pass
+class OakPlaza_Suites_Kumasi(Three_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
 
-class Execuetive_Suite(Room):
-        pass
+		}
 
-class Junior_Suite(Room):
-        pass
+class ThePalms_PramPram(Three_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
 
-class Deluxe_Room(Room):
-		pass
+		}
 
-class Luxury_Room(Room):
-		pass
+class TheAfricanRegent(Three_Star_Hotel):
+	def __init__(self,hotel_name, location, rooms_availability,  room_status):
+		super().__init__(hotel_name, location, rooms_availability)
+		self.room_status = room_status 
+		self.room_price = {"Deluxe Room" : 3000 ,
+							"Luxury Room" : 2000,
+							"Standard Room" : 1000,
+						 }
+							
+						
+		self.room_type = {"Deluxe Room": [1541, 2535, 2501],
+							"Luxury Room": [801, 865, 845],
+							"Standard Room": [101, 108, 136]
 
-class Standard_Room(Room):
-		pass
+		}
 
 
 class HotelMgmtSystem:
---
+++ siliconaccrahotel.py
@@ -0,0 +1,22 @@
+#As a user, I want to input my user info
+# as a user, I wan to to be able to view a list room availability
+# As a user, I want to be able to view roooms by type
+# As a user, I want to be able view rooms by price
+# As a user, i want to be able to book
+
+#Features/Fuctions: + user name, phone, email, checkin date, checkout date
+#                   + view room availability
+#                   + view rooms by type
+#                   + view rooms by price
+#                   + book rooms
+#                   + display reservation confirmation (print random reservation number)
+
+# Design:   ask user for name, phone, email
+#           ask user for date of reservation
+#           ask for number of nights
+#           ask user for room by type
+#           display room requested
+#           display room by price per room
+#           display total price
+
+
